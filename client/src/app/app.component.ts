107dcb2390bfed57410bd16e9626aa6f43500c99
vietanhrs <vietanhtran.uet@gmail.com>
2026-03-16 17:38:28 +0700

    chore: remove dead code from AppComponent
    AppComponent held accountData, sendData(), and an AccountService
    injection left over from a now-deleted inline login form. All credential
    capture is handled by ClickjackOverlayComponent; AppComponent is now
    a plain shell with only the title property.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,36 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService} from './account.service'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-
-  constructor(private accountService: AccountService) {
-
-  }
-
-  ngOnInit(): void {
-
-  }
-
+export class AppComponent {
   title = 'client';
-  accountData = {
-    username: '',
-    password: '',
-  }
-
-  sendData() {
-    this.accountService.createAccount(this.accountData)
-      .subscribe(
-        data => {
-          window.location.href = 'http://iti.vnu.edu.vn/login';
-        },
-        error => {
-          console.log(error);
-        }
-      )
-  }
 }
